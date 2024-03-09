304922cebcee49ce383e1010539de109a19a64d2
Szeng89 <shijinz898@gmail.com>
2024-03-09 15:12:55 -0500

    FINISHED forgot to commit during the process

--- src/main/java/com/zipcodewilmington/PersonHandler.java
+++ src/main/java/com/zipcodewilmington/PersonHandler.java
@@ -16,9 +16,24 @@ public class PersonHandler {
     public String whileLoop() {
         String result = "";
         // create a `counter`
-        // while `counter` is less than length of array
-            // begin loop
+        int counter = 0;
+        String currentPerson;
+        String representation;
 
+        // while `counter` is less than length of array
+        while (counter < personArray.length){
+
+            // we take the valueOf counter and make it into a string then assign it to Representation
+            representation = String.valueOf(personArray[counter]);
+
+            //Concat that value to the result var we made earlier
+            result += representation;
+
+
+            counter++;
+        }
+
+            // begin loop
                 // use `counter` to identify the `current Person` in the array
                 // get `string Representation` of `currentPerson`
                 // append `stringRepresentation` to `result` variable
@@ -31,18 +46,28 @@ public class PersonHandler {
 
     public String forLoop() {
         String result = "";
-        // identify initial value
-        // identify terminal condition
-        // identify increment
+        String representation;
 
         // use the above clauses to declare for-loop signature
             // begin loop
+        for (int i = 0; i < personArray.length; i++){
+
+            //Assign the current value of array into a string then put it into representation
+            representation = String.valueOf(personArray[i]);
+
+            // assign result to result + current representation
+            result += representation;
+
+
+
+        }
                 // use `counter` to identify the `current Person` in the array
                 // get `string Representation` of `currentPerson`
                 // append `stringRepresentation` to `result` variable
             // end loop
 
         return result;
+
     }
 
 
@@ -51,6 +76,18 @@ public class PersonHandler {
         String result = "";
         // identify array's type
         // identify array's variable-name
+
+        // in a for each loop (left = index : right = variable you are looping though)
+        for (Person currentPerson : personArray){
+
+            // Make the valueOf current interation to a string then assign it to names; ** remember in for each loops
+            // the variable you create represents the element of the array no an index.
+             String names = String.valueOf(currentPerson);
+
+             //added each interation of names variable to result ""
+             result += names;
+
+        }
 
         // use the above discoveries to declare for-each-loop signature
             // begin loop
